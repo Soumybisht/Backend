d94e64ecd83ff06949c1edc49b69e50b4028c8c3
Soumy <soumybisht077@gmail.com>
2024-07-24 20:07:47 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//require('dotenv').config({path:'./env'}) //used to make environment variable available for all files
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './env'
+})
+/*
+import express from "express";
+
+const app = express();
+
+ (async () => {
+    try{
+        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("errr",error);
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`server is listening on ${PORT}`);
+        })
+    }
+    catch(error){
+        console.log("Error",error)
+        throw err
+    }
+ })() //it is a efee which is basically execuitng the function immediately after its definiton
+
+ this  is one of the approach to connect to DB.
+ */
+
+ connectDB();
